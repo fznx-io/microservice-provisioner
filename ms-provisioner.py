1736b30be8d7e7e0b71d06299a47497d036789b6
Fauzan <mufahaza@gmail.com>
2019-09-19 10:15:00 +0700

    Add Bitbucket configuration variables

--- ms-provisioner.py
+++ ms-provisioner.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python3
 import requests
 import sys
+
+# Bitbucket Configuration
+bb_url          = 'https://bitbucket.somecompany.com'
+bb_auth_user    = 'bitbucket_auth_user'           # BITBUCKET_AUTH_USER
+bb_auth_pass    = 'bitbucket_auth_password'       # BITBUCKET_AUTH_PASSWORD
+bb_project      = 'MS'
