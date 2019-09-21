a9a46fafb3720c903d2e54bd525e3cbd1dc50002
Fauzan <mufahaza@gmail.com>
2019-09-21 16:45:00 +0700

    Add Jenkins configuration variables

--- ms-provisioner.py
+++ ms-provisioner.py
@@ -7,3 +7,8 @@ bb_url          = 'https://bitbucket.somecompany.com'
 bb_auth_user    = 'bitbucket_auth_user'           # BITBUCKET_AUTH_USER
 bb_auth_pass    = 'bitbucket_auth_password'       # BITBUCKET_AUTH_PASSWORD
 bb_project      = 'MS'
+
+# Jenkins Configuration
+jenkins_url         = 'https://jenkins.somecompany.com'
+jenkins_auth_user   = 'jenkins_auth_user'         # JENKINS_AUTH_USER
+jenkins_auth_pass   = 'jenkins_auth_password'     # JENKINS_AUTH_PASSWORD
