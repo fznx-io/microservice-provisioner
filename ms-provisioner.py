3fb29a61e737048fb816b185129f628a90099939
Fauzan <mufahaza@gmail.com>
2019-09-25 09:30:00 +0700

    Implement Bitbucket repository provisioning logic

--- ms-provisioner.py
+++ ms-provisioner.py
@@ -19,3 +19,51 @@ bb_repo_hook_url        = bb_url + '/rest/api/1.0/projects/' + bb_project + '/re
 bb_branch_perm_url      = bb_url + '/branch-permissions/2.0/projects/' + bb_project + '/repos/{repo}/restrictions'
 bb_merge_check_url      = bb_url + '/rest/split-diff/1.0/projects/' + bb_project + '/repos/{repo}/settings/general'
 bb_merge_strategy_url   = bb_url + '/rest/default-reviewers/1.0/projects/' + bb_project + '/repos/{repo}/condition'
+
+def provision_bitbucket_repo(repo_name):
+    """Create Bitbucket repository with hooks and branch permissions"""
+
+    # Create repository
+    payload = {'name': repo_name}
+    response = requests.post(bb_repo_url, json=payload, auth=(bb_auth_user, bb_auth_pass))
+    print(f'Repository created: {response.status_code}')
+
+    # Enable Jenkins webhook
+    hook_url = bb_repo_hook_url.format(repo=repo_name)
+    hook_payload = {
+        'jenkinsBase': jenkins_url,
+        'gitRepoUrl': '',
+        'ignoreCommitters': '',
+        'ignoreBranches': ''
+    }
+    response = requests.put(hook_url, json=hook_payload, auth=(bb_auth_user, bb_auth_pass))
+    response = requests.put(hook_url + '/enabled', auth=(bb_auth_user, bb_auth_pass))
+    print(f'Jenkins webhook enabled: {response.status_code}')
+
+    # Set branch permissions for develop
+    perm_url = bb_branch_perm_url.format(repo=repo_name)
+    perm_payload = {'type': 'no-deletes', 'matcher': {'id': 'refs/heads/develop', 'type': {'id': 'BRANCH'}}}
+    response = requests.post(perm_url, json=perm_payload, auth=(bb_auth_user, bb_auth_pass))
+    print(f'Develop branch protection: {response.status_code}')
+
+    # Set branch permissions for staging
+    perm_payload = {'type': 'no-deletes', 'matcher': {'id': 'refs/heads/staging', 'type': {'id': 'BRANCH'}}}
+    response = requests.post(perm_url, json=perm_payload, auth=(bb_auth_user, bb_auth_pass))
+    print(f'Staging branch protection: {response.status_code}')
+
+    # Set branch permissions for master
+    perm_payload = {'type': 'no-deletes', 'matcher': {'id': 'refs/heads/master', 'type': {'id': 'BRANCH'}}}
+    response = requests.post(perm_url, json=perm_payload, auth=(bb_auth_user, bb_auth_pass))
+    print(f'Master branch protection: {response.status_code}')
+
+    # Enable merge checks
+    merge_url = bb_merge_check_url.format(repo=repo_name)
+    merge_payload = {'mergeConfig': {'type': 'REPOSITORY', 'strategies': [{'enabled': True, 'flag': 'MERGE_COMMIT', 'id': 'merge-commit'}]}}
+    response = requests.post(merge_url, json=merge_payload, auth=(bb_auth_user, bb_auth_pass))
+    print(f'Merge checks enabled: {response.status_code}')
+
+    # Set merge strategy
+    strategy_url = bb_merge_strategy_url.format(repo=repo_name)
+    strategy_payload = {'mergeConfig': {'defaultStrategy': {'id': 'merge-commit'}}}
+    response = requests.post(strategy_url, json=strategy_payload, auth=(bb_auth_user, bb_auth_pass))
+    print(f'Merge strategy set: {response.status_code}')
